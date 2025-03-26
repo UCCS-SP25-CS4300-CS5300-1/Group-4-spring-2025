a35e6533572b24f5176b4746302638c0dc724297
Gleb Berkez <glebberkez@gmail.com>
2025-03-26 03:43:12 -0600

    working on scrolling and getting access to elements down the window

--- myproject/selenium/login.py
+++ myproject/selenium/login.py
@@ -4,18 +4,40 @@ from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common import keys
+from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Firefox()
 
-driver.implicitly_wait(5)
-driver.get("https://linkedin.com")
 
 username = 'glebberkez@gmail.com'
 password = 'russian333'
+fname = 'Gleb'
+lname = 'Berkez'
+mobile = '719-494-9508'
+email = 'glebberkez@gmail.com'
+
+race = 'White'
+gender = 'Male'
+veteran = 'No'
+disability = 'No'
+linkedInProfile = 'www.linkedin.com/in/glebberkez'
+website = 'www.frontrangejournal.com'
+why_are_you_interested = '?'
+relocation = 'Yes'       # Check area first
+authorization = 'No'
+sponsor = 'No'
+compensation = '$70,000 - $80,000'
+current_location = 'Colorado Springs'
+
+
+driver = webdriver.Firefox()
+driver.implicitly_wait(5)
+driver.get("https://linkedin.com")
+
+
 
 # Try to login
 try:
@@ -56,7 +78,7 @@ except:
     driver.close()
 
 # Send keys to job search
-search_string = 'machine learning intern'
+search_string = 'software engineer'
 try:
     a = driver.find_element(By.XPATH, "//input[@placeholder='Title, skill or company']")
 
@@ -74,20 +96,152 @@ try:
     actions.send_keys(Keys.ENTER).perform()
 except:
     print("could not send keys")
-
-
-# Get list of jobs
-job_titles = []
+time.sleep(3)
+# filter to easy apply
 try:
+    #filters_button = driver.find_element(By.XPATH, "//div[@class='scaffold-layout-toolbar ']/div/section/div/div/div/div/div/button")
+    filters_button = driver.find_element(By.XPATH, "//button[text()='All filters']")
+    print("filters_button", filters_button)
+    filters_button.click()
+    #easy_apply = driver.find_element(By.XPATH, "//div[@class='search-reusables__secondary-filters-filter']/fieldset/div/div")
+    #easy_apply = driver.find_element(By.XPATH, "//input[text()='Toggle Easy Apply filter']")
+    #easy_apply = driver.find_element(By.XPATH, "//legend[text()='Easy Apply filter']")
+
+    #print("easy_apply", easy_apply.get_attribute("class"))
+    #easy_apply_next = driver.find_element(By.XPATH, 'following-sibling::input[1]')
+    #easy_apply_parent = easy_apply.find_element(By.XPATH, "..")
+    #print("easy_apply_parent", easy_apply_parent)
+
+    #easy_apply_toggle = easy_apply_parent.find_element(By.CSS_SELECTOR, "input")
+    easy_apply = driver.find_element(By.XPATH, "//input[@class='input artdeco-toggle__button ']")
+    print("easy_apply", easy_apply.get_attribute("type"))
+
+
+    timeout = 10
+    aria_checked = easy_apply.get_attribute('aria-checked')
+    print("aria_checked", aria_checked)
+    if aria_checked == 'false':
+
+        driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", easy_apply)
+
+        try:
+            # Wait until the element is clickable
+            #element = WebDriverWait(driver, timeout).until(
+            #    EC.element_to_be_clickable(easy_apply)
+            #)
+            #print("element is clickable")
+            driver.execute_script("arguments[0].click();", easy_apply)
+        #time.sleep(10)
+        except:
+            print("Element not clickable")
+    else:
+        print("is it not false")
+
+
+except:
+    print("Vatt are you doing?")
+
+
+
+# Go to show results
+
+time.sleep(3)
+try:
+    show_results = driver.find_element(By.XPATH, "//button[@class='reusable-search-filters-buttons search-reusables__secondary-filters-show-results-button artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")
+    #show_results = driver.find_element(By.XPATH, "//span[text()='Show results']")
+    #show_results_button = show_results.find_element(By.XPATH, "..")
+    show_results.click()
+except:
+    print("Miss clicked")
+time.sleep(3)
+########################################
+########## EASY APPLY READY ############
+########################################
+
+
+
+
+
+
+# Get info, save to database
+# Get list of jobs
+job_list = []
+job_company = []
+company_list = []
+
+try:
+
     unordered_list = driver.find_element(By.XPATH, "//div[@class='scaffold-layout__list ']/div/ul")
     jobs = unordered_list.find_elements(By.XPATH, ".//li/div/div/div/div/div/div/a")
+    jobs2 = unordered_list.find_element(By.XPATH, ".//li")
     for job in jobs:
-        job_titles.append(job.get_attribute("aria-label"))
-        print(job.get_attribute("aria-label"))
+
+        job_title = job.get_attribute("aria-label")
+        link = job.get_attribute("href")
+        company = job.find_element(By.XPATH, "./../../div[2]/span").text
+        location = job.find_element(By.XPATH, "(./../../div)[3]/ul/li/span").text
+        job_list.append(job_title)
+        job_company.append(company)
+        company_list.append(location)
+
+        mydict = { 'job_title': job_title, 'link': link, 'company': company, 'location': location }
+        #job_list.append(mydict)
+
+
+    for i in job_list:
+        print(i)
+    print("--------------\n")
+
+
 
 except NoSuchElementException:
     print("No elements found")
-else:
-    print("length of jobs: ", len(job_titles))
 
-print(job_titles)
+else:
+    print("length of jobs: ", len(job_list))
+
+# Scroll down
+
+iframe = driver.find_element(By.XPATH, "//div[@class='scaffold-layout__list ']")
+scroll_origin = ScrollOrigin.from_element(iframe)
+time.sleep(5)
+ActionChains(driver)\
+    .scroll_from_origin(scroll_origin, 0, 12000)\
+    .perform()
+
+
+
+
+try:
+
+    unordered_list = driver.find_element(By.XPATH, "//div[@class='scaffold-layout__list ']/div/ul")
+    jobs = unordered_list.find_elements(By.XPATH, ".//li/div/div/div/div/div/div/a")
+    for job in jobs:
+
+        job_title = job.get_attribute("aria-label")
+        link = job.get_attribute("href")
+        company = job.find_element(By.XPATH, "./../../div[2]/span").text
+        location = job.find_element(By.XPATH, "(./../../div)[3]/ul/li/span").text
+        job_list.append(job_title)
+        job_company.append(company)
+        company_list.append(location)
+
+        mydict = { 'job_title': job_title, 'link': link, 'company': company, 'location': location }
+        #job_list.append(mydict)
+
+
+    for i in job_list:
+        print(i)
+
+    print("--------------\n")
+
+
+
+except NoSuchElementException:
+    print("No elements found")
+
+else:
+    print("length of jobs: ", len(job_list))
+
+
+
