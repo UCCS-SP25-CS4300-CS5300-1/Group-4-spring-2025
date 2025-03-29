1ca685a4ac1eebf01b72b974ec03c0377b272692
Gleb Berkez <glebberkez@gmail.com>
2025-03-28 20:17:33 -0600

    saving for refactor

--- myproject/selenium/login.py
+++ myproject/selenium/login.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 from selenium import webdriver
 from selenium.common import NoSuchElementException
@@ -51,26 +52,23 @@ try:
     username_input = driver.find_element(By.CSS_SELECTOR, "input[id='username']").send_keys(username)
     password_input = driver.find_element(By.CSS_SELECTOR, "input[id='password']").send_keys(password)
     driver.find_element(By.XPATH, "//button[@type='submit']").click()
-
-
 except:
     print("Could not login")
+
+
 try:
     print("Waiting to be logged in...")
-    #ele = driver.find_element(By.XPATH, "//header[@id='global-nav']//li[3]")
-    #assert ele == 'Jobs'
     enter_credentials = WebDriverWait(driver, 120).until(
         EC.presence_of_all_elements_located((By.XPATH, "//header[@id='global-nav']//li[3]"))
     )
-    print(enter_credentials)
-    print("leaving try block")
+
 except:
     print("time-out")
     print("try again, except sign in quicker")
     driver.close()
 
 
-# Enter jobs
+# Click on Jobs tab in profile page
 try:
     jobs = driver.find_element(By.XPATH, "//header[@id='global-nav']//li[3]")
     jobs.click()
@@ -97,42 +95,27 @@ try:
     actions.send_keys(Keys.ENTER).perform()
 except:
     print("could not send keys")
+
+# Pause for jobs to load
 time.sleep(3)
-# filter to easy apply
+
+# Go to filters and switch on easy apply
 try:
-    #filters_button = driver.find_element(By.XPATH, "//div[@class='scaffold-layout-toolbar ']/div/section/div/div/div/div/div/button")
     filters_button = driver.find_element(By.XPATH, "//button[text()='All filters']")
-    print("filters_button", filters_button)
     filters_button.click()
-    #easy_apply = driver.find_element(By.XPATH, "//div[@class='search-reusables__secondary-filters-filter']/fieldset/div/div")
-    #easy_apply = driver.find_element(By.XPATH, "//input[text()='Toggle Easy Apply filter']")
-    #easy_apply = driver.find_element(By.XPATH, "//legend[text()='Easy Apply filter']")
 
-    #print("easy_apply", easy_apply.get_attribute("class"))
-    #easy_apply_next = driver.find_element(By.XPATH, 'following-sibling::input[1]')
-    #easy_apply_parent = easy_apply.find_element(By.XPATH, "..")
-    #print("easy_apply_parent", easy_apply_parent)
-
-    #easy_apply_toggle = easy_apply_parent.find_element(By.CSS_SELECTOR, "input")
     easy_apply = driver.find_element(By.XPATH, "//input[@class='input artdeco-toggle__button ']")
-    print("easy_apply", easy_apply.get_attribute("type"))
-
 
     timeout = 10
     aria_checked = easy_apply.get_attribute('aria-checked')
-    print("aria_checked", aria_checked)
+
     if aria_checked == 'false':
 
         driver.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", easy_apply)
 
         try:
-            # Wait until the element is clickable
-            #element = WebDriverWait(driver, timeout).until(
-            #    EC.element_to_be_clickable(easy_apply)
-            #)
-            #print("element is clickable")
             driver.execute_script("arguments[0].click();", easy_apply)
-        #time.sleep(10)
+
         except:
             print("Element not clickable")
     else:
@@ -144,16 +127,14 @@ except:
 
 
 
-# Go to show results
-
+# Show results of job search query
 time.sleep(3)
 try:
     show_results = driver.find_element(By.XPATH, "//button[@class='reusable-search-filters-buttons search-reusables__secondary-filters-show-results-button artdeco-button artdeco-button--2 artdeco-button--primary ember-view']")
-    #show_results = driver.find_element(By.XPATH, "//span[text()='Show results']")
-    #show_results_button = show_results.find_element(By.XPATH, "..")
     show_results.click()
 except:
     print("Miss clicked")
+
 time.sleep(3)
 ########################################
 ########## EASY APPLY READY ############
@@ -161,7 +142,24 @@ time.sleep(3)
 
 
 
+# Find maximum number of pages through total number of jobs found
+num_results = driver.find_element(By.XPATH, "//div[@class='scaffold-layout__list ']/header/div/div/small/div/span").text
+print("Num results", num_results)
+num_results = num_results.split()[0]
+time.sleep(5)
+num_results = num_results.split(',')
 
+# If results have a comma (ex. 1,200), then pass the comma to make a string that can be converted to int
+astring = ''
+for i in num_results:
+    if i == ',':
+        pass
+    else:
+        astring += i
+
+# 25 results per page, so round up to get max num of pages
+max_pages = round(int(astring)/25)
+print("max pages", max_pages)
 
 
 # Get info, save to database
@@ -182,6 +180,8 @@ num_pages = 0
 iteration1 = True
 iteration2 = False
 while True:
+
+    # Scroll all the way down
     iframe = driver.find_element(By.XPATH, "//div[@class='scaffold-layout__list ']")
     scroll_origin = ScrollOrigin.from_element(iframe)
     time.sleep(1)
@@ -210,10 +210,12 @@ while True:
         .perform()
 
 
+
+
+    # Start collecting jobs
     try:
 
         unordered_list = driver.find_element(By.XPATH, "//div[@class='scaffold-layout__list ']/div/ul")
-
         jobs = unordered_list.find_elements(By.XPATH, ".//div/div/div/div/div/div/a")
 
         for job in jobs:
@@ -223,12 +225,6 @@ while True:
             location = job.find_element(By.XPATH, "(./../../div)[3]/ul/li/span").text
 
             job_list.append(job_title)
-
-            #company = job.find_element(By.XPATH, "./../../div[2]/span").text
-            #location = job.find_element(By.XPATH, "(./../../div)[3]/ul/li/span").text
-            #job_list.append(job_title)
-            #job_company.append(company)
-            #company_list.append(location)
 
             mydict = { 'job_title': job_title, 'link': link, 'company': company, 'location': location }
             job_list.append(mydict)
@@ -246,78 +242,82 @@ while True:
 
 
     ### Page Change ###
-    unordered_list_pages = driver.find_element(By.XPATH, "//ul[@class='artdeco-pagination__pages artdeco-pagination__pages--number']")
-    pages = unordered_list_pages.find_elements(By.XPATH, ".//li")
+    if max_pages > 1:
+        unordered_list_pages = driver.find_element(By.XPATH, "//ul[@class='artdeco-pagination__pages artdeco-pagination__pages--number']")
+        pages = unordered_list_pages.find_elements(By.XPATH, ".//li")
+        print("Pages", pages)
+        # Get next page
+        page_clicked = False
 
-    # Get next page
-    page_clicked = False
+        while not page_clicked:
+            page_tracker = 1
 
-    while not page_clicked:
-        page_tracker = 1
+            for page in pages:
+                page_number = page.find_element(By.XPATH, ".//button/span").text
 
-        for page in pages:
-            page_number = page.find_element(By.XPATH, ".//button/span").text
+                if iteration1:
 
-            if iteration1:
-
-                # For the 9th page of iteration1, we need to click on the ellipsis
-                if next_page == 9:
-                    # Clicked to next set of pages. Iteration 1 is over
-                    print("next_page", next_page)
-                    print("page number", page_number)
-                    page.find_element(By.XPATH, "./button").click()
-                    time.sleep(2)
-
-                    # Change variables
-                    current_page += 1
-                    next_page += 1
-                    page_clicked = True
-
-                    # Now, only iteration2 will be in effect
-                    iteration1 = False
-                    iteration2 = True
-                    # Exit out of page_clicked while loop
-                    break
-
-                else:
-                    if int(page_number) == next_page:
-                        page.find_element(By.XPATH, ".//button").click()
+                    # For the 9th page of iteration1, we need to click on the ellipsis
+                    if next_page == 9:
+                        # Clicked to next set of pages. Iteration 1 is over
+                        print("next_page", next_page)
+                        print("page number", page_number)
+                        page.find_element(By.XPATH, "./button").click()
                         time.sleep(2)
 
                         # Change variables
                         current_page += 1
                         next_page += 1
-
-                        # Exit out of page_clicked while loop
                         page_clicked = True
+
+                        # Now, only iteration2 will be in effect
+                        iteration1 = False
+                        iteration2 = True
+                        # Exit out of page_clicked while loop
                         break
-            elif iteration2:
 
-                # Skip over the first ellipsis in all subsequent iterations
-                if page_tracker <= 3:
-                    page_tracker += 1
-                    continue
+                    else:
+                        if int(page_number) == next_page:
+                            page.find_element(By.XPATH, ".//button").click()
+                            time.sleep(2)
 
-                # Capture second ellipsis
-                if page_tracker == 9:
-                    page.find_element(By.XPATH, "./button").click()
-                    time.sleep(2)
+                            # Change variables
+                            current_page += 1
+                            next_page += 1
 
-                    # Change variables
-                    current_page += 1
-                    next_page += 1
-                    page_clicked = True
-                    # Break out of while loop
-                    break
+                            # Exit out of page_clicked while loop
+                            page_clicked = True
+                            break
+                elif iteration2:
 
-                if int(page_number) == next_page:
-                    page.find_element(By.XPATH, "./button").click()
-                    time.sleep(2)
+                    # Skip over the first ellipsis in all subsequent iterations
+                    if page_tracker < 3:
+                        page_tracker += 1
+                        continue
 
-                    # Change variables
-                    current_page += 1
-                    next_page += 1
-                    page_clicked = True
-                    # Break out of while loop
-                    break
-            page_tracker += 1
+                    # Capture second ellipsis
+                    if page_tracker == 9:
+                        page.find_element(By.XPATH, "./button").click()
+                        time.sleep(2)
+
+                        # Change variables
+                        current_page += 1
+                        next_page += 1
+                        page_clicked = True
+                        # Break out of while loop
+                        break
+
+                    if int(page_number) == next_page:
+                        page.find_element(By.XPATH, "./button").click()
+                        time.sleep(2)
+
+                        # Change variables
+                        current_page += 1
+                        next_page += 1
+                        page_clicked = True
+                        # Break out of while loop
+                        break
+                page_tracker += 1
+    else:
+
+        print("No need to continue")
