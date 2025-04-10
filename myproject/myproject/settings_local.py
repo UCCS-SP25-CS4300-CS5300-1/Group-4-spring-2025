f10872ee5868f42478017058c3d18a9441ff4c45
Aziz Alghamdi <123228478+AzizAlghamdi@users.noreply.github.com>
2025-04-09 23:49:17 -0600

    Delete myproject/myproject/settings_local.py

--- myproject/myproject/settings_local.py
+++
@@ -1,5 +0,0 @@
-# Local settings file - DO NOT commit this to version control!
-# This file overrides settings in the main settings.py file
-
-# OpenAI API Key
-OPENAI_API_KEY = "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"
