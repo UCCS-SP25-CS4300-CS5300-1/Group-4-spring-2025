9f5850c1f30d1cb125b8e67ac5bb3de8aae95722
bdamja <e>
2025-04-14 21:23:52 -0600

    wip, need to finish template, load page after resume is read, then a button to generate rejection reasons

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('interview-coach/', views.interview_coach, name='interview_coach'),
     path('interview-coach/<str:job_id>/', views.interview_coach, name='interview_coach_with_job'),
+    path('rejection-simulator/<str:job_id>/', views.rejection_reason_simulator, name='rejection_simulator'),
     path('api/evaluate-response/', views.ajax_evaluate_response, name='evaluate_response'),
     path('api/generate-questions/', views.ajax_generate_questions, name='generate_questions'),
     path('applications/', views.applications, name='applications'),
